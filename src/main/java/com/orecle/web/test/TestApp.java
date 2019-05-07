df36b213914f91ab1cba1d84e493ff04fdc4e119
WangManli <WangManli@qq.com>
2019-05-07 11:14:32 +0800

    添加了一个TestApp

---
+++ src/main/java/com/orecle/web/test/TestApp.java
@@ -0,0 +1,13 @@
+package com.orecle.web.test;
+
+public class TestApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello");
+		
+
+	}
+
+}
