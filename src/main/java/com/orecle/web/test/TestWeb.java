a32a09a57cfb741a4276a447af404a96677750ac
WangManli <WangManli@qq.com>
2019-05-07 11:16:32 +0800

    添加了一个TestWeb类

---
+++ src/main/java/com/orecle/web/test/TestWeb.java
@@ -0,0 +1,12 @@
+package com.orecle.web.test;
+
+public class TestWeb {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello");
+
+	}
+
+}
